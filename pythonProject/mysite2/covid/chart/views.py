e3d7ba42535bfbc67b6e746a48548a643163c31f
HUH-HOON <66860841+HUH-HOON@users.noreply.github.com>
2020-09-17 17:02:44 +0900

    Initial commit

---
+++ pythonProject/mysite2/covid/chart/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+import pandas as pd
+
+def main_view(request):
+    df = pd.read_csv(r'C:\Users\Playdata\Downloads\gangnam.csv')
+    labels = df['STDR_DE_ID'].tolist()
+    data =  df['TOT_LVPOP_CO'].values.tolist()
+    return render(request, 'line-chart.html', {
+        'labels' : labels,
+        'data' : data,
+
+    })
+
